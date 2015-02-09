93ed235bf8454fba2b86ad2745550db0107ed8c6
Lucas BARBAY <lucas.barbay@gmail.com>
2015-02-09 09:58:10 +0100

    modification fichiers suppression de clefs

--- GuiPG/src/View/keysuppression.cpp
+++ GuiPG/src/View/keysuppression.cpp
@@ -1,5 +1,7 @@
 #include "keysuppression.h"
 #include "ui_keysuppression.h"
+#include "../action.h"
+
 
 KeySuppression::KeySuppression(QWidget *parent) :
     QDialog(parent),
@@ -11,4 +13,22 @@ KeySuppression::KeySuppression(QWidget *parent) :
 KeySuppression::~KeySuppression()
 {
     delete ui;
+}
+
+
+void KeySuppression::on_pushButton_clicked()
+{
+    close();
+}
+
+
+void KeySuppression::on_pushButton_2_clicked()
+{
+    //Action KeySuppression(QString("--delete-keys"), QStringList(), QStringList(), interactions);
+    // TODO Donner l'action à gpg
+    // TODO Comment rentrer la phrase de passe ?
+    //errorLabel->setText("Veuillez patienter pendant la suppression de la clef...\n");
+
+    // TODO Attendre que GPG ait fini de supprimer la clef
+    //close();
 }
--- GuiPG/src/View/keysuppression.h
+++ GuiPG/src/View/keysuppression.h
@@ -15,6 +15,11 @@ public:
     explicit KeySuppression(QWidget *parent = 0);
     ~KeySuppression();
 
+private slots:
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_clicked();
+
 private:
     Ui::KeySuppression *ui;
 };
