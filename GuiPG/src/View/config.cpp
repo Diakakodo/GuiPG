7806a18e7675e477d624672fbdfeeb4622218001
ibrahima <ibrahima_barry@netcourrier.com>
2015-02-17 01:02:10 +0100

    mise a jour du profil par default

--- GuiPG/src/View/config.cpp
+++ GuiPG/src/View/config.cpp
@@ -1,22 +1,36 @@
 #include "config.h"
 #include "ui_config.h"
 #include <QFileDialog>
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
 
 config::config(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::config)
 {
     ui->setupUi(this);
-}
 
+    struct passwd *pw = getpwuid(getuid());
+    QString homedir = pw->pw_dir;
+    QString home = "home/";
+
+    ui->lineEdit_2->setText((homedir).append(".gnugp"));
+
+}
 config::~config()
+
 {
     delete ui;
 }
 
 void config::on_pushButton_clicked()
 {
-  QString filename=QFileDialog::getOpenFileName(this,tr("Open File"),"home/ibrahima","All files (*.*)" );
+  struct passwd *pw = getpwuid(getuid());
+  const char *homedir = pw->pw_dir;
+  QString home = "home/";
+
+  QString filename=QFileDialog::getOpenFileName(this,tr("Open File"),homedir,"All files (*.*)" );
   ui->lineEdit_2->setText(filename);
 
 };
