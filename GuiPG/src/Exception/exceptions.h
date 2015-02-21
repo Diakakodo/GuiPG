d5a89ee8158ffd9049a233cedd958466fb84181a
MatthieuFin <matthieu2717@gmail.com>
2015-02-21 22:20:43 +0100

    Ajout d'un header permettant d'inclure tout les types d'exceptions en un seul header ...

--- GuiPG/src/Exception/exceptions.h
+++ GuiPG/src/Exception/exceptions.h
@@ -1,5 +1,13 @@
 #ifndef EXCEPTIONS
 #define EXCEPTIONS
 
+/**
+ * Fichier permettant uniquement d'inclure toutes les exceptions de
+ * l'application en un seul fichier.
+ */
+
+#include "illegalargumentexception.h"
+#include "illegalstateexception.h"
+
 #endif // EXCEPTIONS
 
