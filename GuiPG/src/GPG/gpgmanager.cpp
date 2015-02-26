afdb57cec8e49c07d343eeaeccc2615f4ff4280e
MatthieuFin <matthieu2717@gmail.com>
2015-02-26 18:13:30 +0100

    Ajout de la gestion des interactions gpg

--- GuiPG/src/GPG/gpgmanager.cpp
+++ GuiPG/src/GPG/gpgmanager.cpp
@@ -5,6 +5,47 @@ GPGManager::GPGManager(const Profile *p) : m_profile(p) {
     connect(&m_gpg, (void (QProcess::*)(int, QProcess::ExitStatus)) &QProcess::finished,
             this, &GPGManager::terminate);
     connect(&m_gpg, &QProcess::readyReadStandardOutput, this, &GPGManager::readOutput);
+    connect(&m_gpg, (void (QProcess::*)(QProcess::ProcessError)) &QProcess::error, this, &GPGManager::errorGPG);
+    connect(&m_gpg, &QProcess::stateChanged, this, &GPGManager::stateChanged);
+
+    m_gpg.setProcessChannelMode(QProcess::MergedChannels);
+    m_gpg.setReadChannel(QProcess::StandardOutput);
+}
+
+bool GPGManager::askInteraction() {
+    QStringList l = m_output.split('\n', QString::SkipEmptyParts);
+    QString last = l.last();
+    if (last.contains("[GNUPG:] GET_LINE")) {
+        return true;
+    }
+    return false;
+}
+
+void GPGManager::sendInteraction() {
+    if (m_gpg.state() != QProcess::Running
+            || !askInteraction()) {
+        throw GPGException("m_gpg.state() != QProcess::Running "
+                           "|| !askInteraction(m_output)");
+    }
+    if (!m_action.hasInteraction()) {
+        m_gpg.kill();
+        throw NotEnoughInteraction("Pas assez d'interactions.");
+    } else {
+        QByteArray data(m_action.nextInteraction().toLatin1());
+        m_output += data;
+        m_gpg.write(data);
+    }
+}
+
+void GPGManager::stateChanged(QProcess::ProcessState newState) {
+    if (newState == QProcess::Running
+            && m_action.hasInteraction()
+            && askInteraction()) {
+        sendInteraction();
+    }
+}
+
+void GPGManager::errorGPG(QProcess::ProcessError error) {
 }
 
 void GPGManager::execute() {
@@ -12,13 +53,22 @@ void GPGManager::execute() {
     if (m_profile->getConfigurationPath() != "") {
         args << "--homedir" << m_profile->getConfigurationPath();
     }
-    args << m_action.getOptions() << "--with-colons" << m_action.getCmd()
+    args << m_action.getOptions() << m_action.getCmd()
          << m_action.getArgs();
+    qDebug() << m_profile->getGPGExecutable();
+    qDebug() << args;
+
     m_gpg.start(m_profile->getGPGExecutable(), args);
 }
 
 void GPGManager::readOutput() {
-    m_output += m_gpg.readAllStandardOutput();
+    QString tmp = m_gpg.readAllStandardOutput();
+    //qDebug() << tmp;
+    m_output += tmp;
+    if (m_gpg.state() == QProcess::Running
+            && askInteraction()) {
+        sendInteraction();
+    }
 }
 
 const QString& GPGManager::getOutput() const {
--- GuiPG/src/GPG/gpgmanager.h
+++ GuiPG/src/GPG/gpgmanager.h
@@ -5,6 +5,7 @@
 #include <QProcess>
 #include "../Profile/profile.h"
 #include "action.h"
+#include <QSemaphore>
 
 class GPGManager : public QObject {
         Q_OBJECT
@@ -22,12 +23,28 @@ class GPGManager : public QObject {
     private slots:
         void readOutput();
         void terminate(int s, QProcess::ExitStatus status);
+        void errorGPG(QProcess::ProcessError error);
+        void stateChanged(QProcess::ProcessState newState);
 
     private:
         QProcess m_gpg;
         const Profile* m_profile;
         QString m_output;
         Action m_action;
+
+        /**
+         * @brief sendInteraction
+         * @pre
+         *      m_gpg.state() == QProcess::Running
+         *   && askInteraction()
+         */
+        void sendInteraction();
+        /**
+         * @brief askInteraction Permet de savoir si le retour de GPG necessite
+         * une interaction ou non.
+         * @return un booléen indiquant si gpg attend des données.
+         */
+        bool askInteraction();
 };
 
 #endif // GPGMANAGER_H
