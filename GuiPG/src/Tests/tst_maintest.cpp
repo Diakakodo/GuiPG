1bdc788b98c90e08d2108c8c14ae4fab955f0d89
Pierre Balmelle <pierre.balmelle@gmail.com>
2015-04-07 21:31:20 +0200

    Ajout des tests pour la livraison de jeudi + màj CDR

--- GuiPG/src/Tests/tst_maintest.cpp
+++ GuiPG/src/Tests/tst_maintest.cpp
@@ -1,12 +1,15 @@
 #include <QString>
 #include <QtTest>
 #include "../Profile/profile.h"
+#include "../Profile/profilemanager.h"
 #include "../Configuration/configuration.h"
 #include "../GPG/action.h"
 #include "../Model/mainwindowmodel.h"
 #include "../View/mainwindow.h"
 #include "../View/keyexport.h"
 #include "../View/Profil/profilecreation.h"
+#include "../Launcher/launcher.h"
+#include "../GPG/gpgmanager.h"
 #include "stdlib.h"
 #include <stdio.h>
 #include "unistd.h"
@@ -25,8 +28,12 @@ private:
     GuiPGApp* m_app;
 
 private Q_SLOTS:
-    void testCase_u11();
-    void testCase_u13();
+    void testCase_u3();
+    void testCase_u5();
+    void testCase_u6();
+    void testCase_u7();
+    void testCase_u8();
+    void testCase_u10();
     void testCase_nr1();
     void testCase_nr2();
     void testLoadConfig();
@@ -44,20 +51,48 @@ MainTest::MainTest()
     m_app = app;
 }
 
-void MainTest::testCase_u11()
+void MainTest::testCase_u3()
+{
+    QList<Profile*> profiles = m_config->getProfiles();
+    m_config->addProfile(new Profile(1751, "test"));
+    QVERIFY(profiles.length() == m_config->getProfiles().length() - 1);
+    QVERIFY(m_config->getProfileById(1751) != nullptr);
+    m_config->deleteProfile(1751);
+    QVERIFY(m_config->getProfileById(1751) == nullptr);
+    QVERIFY(profiles == m_config->getProfiles());
+}
+
+void MainTest::testCase_u5()
 {
     Profile p(66, "Mister T");
     p.setConfigurationPath("/");
     QString s = p.getConfigurationPath();
     QVERIFY(p.getConfigurationPath() == "/");
     p.setGPGExecutable("/");
-    s = p.getGPGExecutable();
     QVERIFY(p.getGPGExecutable() == "/");
     QVERIFY(p.getId() == 66);
     QVERIFY(p.getName() == "Mister T");
 }
 
-void MainTest::testCase_u13()
+void MainTest::testCase_u6()
+{
+    Launcher launcher(m_app, m_config);
+    QVERIFY(launcher.alreadyRun() == false);
+    MainWindowModel model(&launcher, m_app, m_config, m_config->getDefaultProfile());
+    MainWindow mainWindow(&model);
+    //QVERIFY_EXCEPTION_THROWN(launcher.run(), IllegalArgumentException);
+    //QVERIFY_EXCEPTION_THROWN(launcher.addMainWindow(NULL, &window), IllegalArgumentException);
+    //QVERIFY_EXCEPTION_THROWN(launcher.addMainWindow(m_config->getDefaultProfile(), NULL), IllegalArgumentException);
+    launcher.addMainWindow(m_config->getDefaultProfile(), &mainWindow);
+    launcher.run();
+    QVERIFY(launcher.alreadyRun() == true);
+    QVERIFY(launcher.profileIsLoad(m_config->getDefaultProfile()) != NULL);
+    launcher.UnloadProfileWithWindow(m_config->getDefaultProfile());
+    launcher.stop();
+    QVERIFY(launcher.alreadyRun() == true);
+}
+
+void MainTest::testCase_u7()
 {
     QStringList interactions = (QStringList() << "1" << "2" << "3");
     Action a("test", QStringList(), QStringList(), interactions);
@@ -65,11 +100,37 @@ void MainTest::testCase_u13()
     QVERIFY(a.getArgs() == QStringList());
     QVERIFY(a.getOptions() == QStringList());
     QVERIFY(a.getInteractions() == interactions);
-    QVERIFY(a.nextInteraction() == "1");
-    QVERIFY(a.nextInteraction() == "2");
-    QVERIFY(a.nextInteraction() == "3");
+    QVERIFY(a.nextInteraction() == "1\n");
+    QVERIFY(a.nextInteraction() == "2\n");
+    QVERIFY(a.nextInteraction() == "3\n");
     //Vérifier que a.nextInteraction() lance une exception (qt 5.3 requis)
     //QVERIFY_EXCEPTION_THROWN(a.nextInteraction(), IllegalStateException);
+}
+
+void MainTest::testCase_u8()
+{
+    GPGManager manager(m_config->getDefaultProfile());
+    Action a("--version", QStringList(), QStringList());
+    manager.setAction(a);
+    QEventLoop loop;
+    connect(&manager, &GPGManager::finished, &loop, &QEventLoop::quit);
+    manager.execute();
+    loop.exec();
+    QVERIFY(manager.getOutput() != "");
+}
+
+void MainTest::testCase_u10()
+{
+    Launcher launcher(m_app, m_config, m_config->getDefaultProfileId());
+    MainWindowModel model(&launcher, m_app, m_config, m_config->getDefaultProfile());
+    QVERIFY(model.getLauncher() == &launcher);
+    QVERIFY(model.getGuiPGApp() == m_app);
+    QVERIFY(model.getConf() == m_config);
+    QVERIFY(model.getProfile() == m_config->getDefaultProfile());
+    MainWindow mainWindow(&model);
+    model.loadProfile(m_config->getDefaultProfileId(), &mainWindow);
+    model.initKeyManager();
+    QVERIFY(model.getKeyManager() != NULL);
 }
 
 void MainTest::testCase_nr1()
