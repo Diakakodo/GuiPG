959d46e0634d45b3bfba630d72d5ebae9dc302f8
Pierre Balmelle <pierre.balmelle@gmail.com>
2015-04-13 19:06:13 +0200

    Màj CDR + 1 nouveau test unitaire d'exportation

--- GuiPG/src/Tests/tst_maintest.cpp
+++ GuiPG/src/Tests/tst_maintest.cpp
@@ -34,6 +34,7 @@ private Q_SLOTS:
     void testCase_u7();
     void testCase_u8();
     void testCase_u10();
+    void testCase_u11();
     void testCase_nr1();
     void testCase_nr2();
     void testLoadConfig();
@@ -128,6 +129,21 @@ void MainTest::testCase_u10()
     model.loadProfile(m_config->getDefaultProfileId(), &mainWindow);
     model.initKeyManager();
     QVERIFY(model.getKeyManager() != NULL);
+}
+
+void MainTest::testCase_u11()
+{
+    remove("/tmp/TEST");
+    Launcher launcher(m_app, m_config, m_config->getDefaultProfileId());
+    MainWindowModel model(&launcher, m_app, m_config, m_config->getDefaultProfile());
+    MainWindow mainWindow(&model);
+    KeyExport* keyExport = new KeyExport(&mainWindow);
+    keyExport->exportFunction(KeyExport::EXPORT_FILE, "", "/tmp/TEST");
+    QVERIFY(remove("/tmp/TEST") != -1);
+    QVERIFY(keyExport->exportFunction(KeyExport::EXPORT_FILE, "", "/tmp/gaijgz/kjqnonzb/zzozop") == -1);
+    QVERIFY(remove("/tmp/gaijgz/kjqnonzb/zzozop") == -1);
+    QVERIFY(keyExport->exportFunction(KeyExport::EXPORT_FILE, "", "/etc/test") == -1);
+    QVERIFY(remove("/etc/test") == -1);
 }
 
 void MainTest::testCase_nr1()
