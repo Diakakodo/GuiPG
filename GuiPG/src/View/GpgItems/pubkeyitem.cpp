ab3622074a054932a9e1d286b8d362783ed2cedd
MatthieuFin <matthieu2717@gmail.com>
2015-04-16 12:37:51 +0200

    Ajout de la base de la vue des clé publiques

--- GuiPG/src/View/GpgItems/pubkeyitem.cpp
+++ GuiPG/src/View/GpgItems/pubkeyitem.cpp
@@ -1,8 +1,19 @@
 #include "pubkeyitem.h"
 
-PubKeyItem::PubKeyItem()
-{
 
+#include <QMenu>
+
+PubKeyItem::PubKeyItem(PubKey* pub) : GpgItem(pub)
+{
+    m_pub = pub;
+
+    setText(COL_ALGO, pub->getAlgo());
+    setText(COL_CAPACITY, pub->getCapabilities());
+    setText(COL_CREATION, pub->getCreationDate().toString("dd/MM/yyyy"));
+    setText(COL_EXPIRATION, pub->getExpirationDate().toString("dd/MM/yyyy"));
+    setText(COL_ID, pub->getKeyId());
+    setText(COL_LENGTH, QString::number(pub->getLength()));
+    setText(COL_VALIDITY, pub->getValidity());
 }
 
 PubKeyItem::~PubKeyItem()
@@ -10,3 +21,8 @@ PubKeyItem::~PubKeyItem()
 
 }
 
+void PubKeyItem::showMenu(const QPoint &pos) {
+    QMenu* menu = new QMenu(treeWidget());
+    menu->addAction("Test Clef public");
+    menu->popup(treeWidget()->viewport()->mapToGlobal(pos));
+}
--- GuiPG/src/View/GpgItems/pubkeyitem.h
+++ GuiPG/src/View/GpgItems/pubkeyitem.h
@@ -1,12 +1,19 @@
 #ifndef PUBKEYITEM_H
 #define PUBKEYITEM_H
 
+#include "../../Keys/pubkey.h"
+#include "gpgitem.h"
 
-class PubKeyItem
+class PubKeyItem : public GpgItem
 {
-public:
-    PubKeyItem();
-    ~PubKeyItem();
+    public:
+        PubKeyItem(PubKey* pud);
+        ~PubKeyItem();
+        void showMenu(const QPoint &pos);
+
+    private:
+        PubKey* m_pub;
+
 };
 
 #endif // PUBKEYITEM_H
