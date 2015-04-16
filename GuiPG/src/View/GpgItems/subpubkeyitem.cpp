7b2c2e20db3388ec746f86e01c7feddaa01e0013
MatthieuFin <matthieu2717@gmail.com>
2015-04-16 12:15:01 +0200

    Ajout de la base d'un SubPubKeyItem

--- GuiPG/src/View/GpgItems/subpubkeyitem.cpp
+++ GuiPG/src/View/GpgItems/subpubkeyitem.cpp
@@ -1,8 +1,21 @@
 #include "subpubkeyitem.h"
 
-SubPubKeyItem::SubPubKeyItem()
-{
+#include <QMenu>
 
+#include "../../Keys/signature.h"
+#include "signatureitem.h"
+
+SubPubKeyItem::SubPubKeyItem(SubPubKey* sub) : GpgItem(sub)
+{
+    m_sub = sub;
+    setText(COL_ALGO, sub->getAlgo());
+    setText(COL_CREATION, sub->getCreationDate().toString("dd/MM/yyyy"));
+    setText(COL_ID, sub->getKeyId());
+    setText(COL_LENGTH, QString::number(sub->getLength()));
+    setText(COL_VALIDITY, sub->getValidity());
+    for (Signature* sig : sub->getSignatures()) {
+        addChild(new SignatureItem(sig));
+    }
 }
 
 SubPubKeyItem::~SubPubKeyItem()
@@ -10,3 +23,9 @@ SubPubKeyItem::~SubPubKeyItem()
 
 }
 
+
+void SubPubKeyItem::showMenu(const QPoint &pos) {
+    QMenu* menu = new QMenu(treeWidget());
+    menu->addAction("Test Sous clef public");
+    menu->popup(treeWidget()->viewport()->mapToGlobal(pos));
+}
--- GuiPG/src/View/GpgItems/subpubkeyitem.h
+++ GuiPG/src/View/GpgItems/subpubkeyitem.h
@@ -1,12 +1,17 @@
 #ifndef SUBPUBKEYITEM_H
 #define SUBPUBKEYITEM_H
 
+#include "gpgitem.h"
+#include "../../Keys/subpubkey.h"
 
-class SubPubKeyItem
+class SubPubKeyItem : public GpgItem
 {
-public:
-    SubPubKeyItem();
-    ~SubPubKeyItem();
+    public:
+        SubPubKeyItem(SubPubKey* sub);
+        ~SubPubKeyItem();
+        void showMenu(const QPoint &pos);
+    private:
+        SubPubKey* m_sub;
 };
 
 #endif // SUBPUBKEYITEM_H
