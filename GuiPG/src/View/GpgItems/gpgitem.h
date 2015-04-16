baaa0a701ac042b925852c8cfe60ec723d3fe9f8
MatthieuFin <matthieu2717@gmail.com>
2015-04-16 10:54:00 +0200

    Ajout d'une X_MACRO et d'une hashmap dans gpgItem pour abstraire un peut le nom et l'ordre des colones pour le treewidget des clés

--- GuiPG/src/View/GpgItems/gpgitem.h
+++ GuiPG/src/View/GpgItems/gpgitem.h
@@ -5,6 +5,20 @@
 #include "../../Keys/gpgobject.h"
 #include <QPoint>
 
+// X(nom de l'enum, nom de la colone)
+#define X_COLUMNS               \
+X(NAME_COL,       "Nom")        \
+X(MAIL_COL,       "Email")      \
+X(VALIDITY_COL,   "Validité")   \
+X(TRUST_COL,      "Confiance")  \
+X(CREATION_COL,   "Création")   \
+X(EXPIRATION_COL, "Expiration") \
+X(LENGTH_COL,     "Taille")     \
+X(ALGO_COL,       "Algo")       \
+X(CAPACITY_COL,   "Capacité")   \
+X(ID_COL,         "ID")
+
+
 class GpgItem : public QTreeWidgetItem {
 
     public:
@@ -14,6 +28,18 @@ class GpgItem : public QTreeWidgetItem {
         virtual ~GpgItem() = 0;
 
         virtual void showMenu(const QPoint &pos) = 0;
+
+        // Cette enum permet de conter le nombre de colones
+        // définie et de leur attribuer un numéro selon leur ordre de définition.
+        #define X(id, str) id,
+        enum columnsNumber {
+            X_COLUMNS
+            NB_COLUMNS
+        };
+        #undef X
+
+
+        static QHash<int, QString> columns;
 
     protected:
         GpgObject* m_gpgObject;
