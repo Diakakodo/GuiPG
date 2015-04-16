129a9436dda7b0fc7e166ce42c3bd7cc34790c88
MatthieuFin <matthieu2717@gmail.com>
2015-04-16 16:22:13 +0200

    Ajout de la base de la vue des PrimaSecKeyItem

--- GuiPG/src/View/GpgItems/primaseckeyitem.cpp
+++ GuiPG/src/View/GpgItems/primaseckeyitem.cpp
@@ -1,8 +1,19 @@
 #include "primaseckeyitem.h"
+#include "uiditem.h"
+#include <QMenu>
 
-PrimaSecKeyItem::PrimaSecKeyItem()
+PrimaSecKeyItem::PrimaSecKeyItem(PrimaSecKey *sec) : SubSecKeyItem(sec)
 {
+    m_sec = sec;
 
+    setText(COL_EXPIRATION, sec->getExpirationDate().toString("dd/MM/yyyy"));
+    Uid* uid = sec->getUid();
+    if (uid != nullptr) {
+        addChild(new UidItem(uid));
+    }
+    for (SubSecKey* ssc : sec->getSubSecKeyList()) {
+        addChild(new SubSecKeyItem(ssc));
+    }
 }
 
 PrimaSecKeyItem::~PrimaSecKeyItem()
@@ -10,3 +21,8 @@ PrimaSecKeyItem::~PrimaSecKeyItem()
 
 }
 
+void PrimaSecKeyItem::showMenu(const QPoint &pos) {
+    QMenu* menu = new QMenu(treeWidget());
+    menu->addAction("Test Clef Privée primaire");
+    menu->popup(treeWidget()->viewport()->mapToGlobal(pos));
+}
--- GuiPG/src/View/GpgItems/primaseckeyitem.h
+++ GuiPG/src/View/GpgItems/primaseckeyitem.h
@@ -1,12 +1,19 @@
 #ifndef PRIMASECKEYITEM_H
 #define PRIMASECKEYITEM_H
 
+#include "subseckeyitem.h"
+#include "../../Keys/primaseckey.h"
 
-class PrimaSecKeyItem
+class PrimaSecKeyItem : public SubSecKeyItem
 {
-public:
-    PrimaSecKeyItem();
-    ~PrimaSecKeyItem();
+    public:
+        PrimaSecKeyItem(PrimaSecKey* sec);
+        ~PrimaSecKeyItem();
+
+        void showMenu(const QPoint &pos);
+
+    private:
+        PrimaSecKey* m_sec;
 };
 
 #endif // PRIMASECKEYITEM_H
