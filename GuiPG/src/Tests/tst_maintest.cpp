8ba5baed282119082f76239be5d3a77253e34b4f
Sam <pierre.balmelle@gmail.com>
2015-04-22 15:24:56 +0200

    Tests : Màj finale

--- GuiPG/src/Tests/tst_maintest.cpp
+++ GuiPG/src/Tests/tst_maintest.cpp
@@ -5,6 +5,8 @@
 #include "../Configuration/configuration.h"
 #include "../GPG/action.h"
 #include "../Model/mainwindowmodel.h"
+#include "../Keys/primaseckey.h"
+#include "../Keys/primapubkey.h"
 #include "../View/mainwindow.h"
 #include "../View/keyexport.h"
 #include "../View/keyimport.h"
@@ -31,23 +33,17 @@ private:
     MainWindow* m_mainWindow;
     MainWindowModel* m_model;
     Launcher* m_launcher;
-    KeyImport* m_import;
-    //A FAIRE A LA MAIN
-    void testCase_nr2();
-    void testCase_u11();
-    void testCase_u12();
-    void testCase_nr1();
-    //A FAIRE A LA MAIN
+
 
 private Q_SLOTS:
     void testCase_u1();
+    void testCase_u2();
     void testCase_u3();
+    void testCase_u4();
     void testCase_u5();
     void testCase_u6();
     void testCase_u7();
     void testCase_u8();
-    void testCase_u10();
-    void testLoadConfig();
     void cleanupTestCase();
 };
 
@@ -73,16 +69,10 @@ MainTest::MainTest()
     m_model = model;
     MainWindow* mainWindow= new MainWindow(m_model);
     m_mainWindow = mainWindow;
-    KeyImport* import = new KeyImport(m_mainWindow);
-    m_import = import;
 }
+
 
 void MainTest::testCase_u1()
-{
-    m_model->getKeyManager()->getPubKeys();
-}
-
-void MainTest::testCase_u3()
 {
     QList<Profile*> profiles = m_config->getProfiles();
     m_config->addProfile(new Profile(1751, "test"));
@@ -91,36 +81,59 @@ void MainTest::testCase_u3()
     m_config->deleteProfile(1751);
     QVERIFY(m_config->getProfileById(1751) == nullptr);
     QVERIFY(profiles == m_config->getProfiles());
+    qDebug() << "Classes de gestion de profil : OK!";
 }
 
-void MainTest::testCase_u5()
+void MainTest::testCase_u2()
+{
+    PrimaSecKey primaSecKey(QString("1"), 1024, QString("aaa"), QString("6666666"), QDate(1,2,3), QDate(4,5,6));
+    QVERIFY(primaSecKey.getExpirationDate() == QDate(4,5,6));
+    Uid* uid = new Uid("1", QDate(1,2,3), QString(""), QString("bob"), QString("bob@bob.bob"));
+    primaSecKey.addUid(uid);
+    QVERIFY(primaSecKey.getUidList().contains(uid));
+    QVERIFY(primaSecKey.hasPrimaSecKey() == false);
+    PrimaPubKey* pubKey = new PrimaPubKey(QString("1"), QString("1"), 10, QString("RSA"), QString("66"), QDate(1,2,3), QDate(2,3,4), QString(""), QString(""));
+    primaSecKey.setPrimaryPubKey(pubKey);
+    QVERIFY(primaSecKey.hasPrimaSecKey() == true);
+    QVERIFY(primaSecKey.getPrimarySecKey() == pubKey);
+    SubSecKey* subSecKey = new SubSecKey(QString(""), 10, QString(), QString("15"), QDate(52,15,17));
+    primaSecKey.addSubSecKey(subSecKey);
+    QVERIFY(primaSecKey.getSubSecKeyList().contains(subSecKey));
+    delete uid;
+    delete pubKey;
+    qDebug() << "Classe PrimaSecKey : OK!";
+}
+
+void MainTest::testCase_u3()
 {
     Profile p(66, "Mister T");
     p.setConfigurationPath("/");
     QString s = p.getConfigurationPath();
     QVERIFY(p.getConfigurationPath() == "/");
-    p.setGPGExecutable("/");
-    QVERIFY(p.getGPGExecutable() == "/");
+    p.setGPGExecutable("gpg");
+    QVERIFY(p.getGPGExecutable() == "gpg");
     QVERIFY(p.getId() == 66);
     QVERIFY(p.getName() == "Mister T");
+    qDebug() << "Classe Profile : OK!";
 }
 
-void MainTest::testCase_u6()
+void MainTest::testCase_u4()
 {
     Launcher launcher(m_app, m_config);
+    //qt 5.3 requis
     //QVERIFY_EXCEPTION_THROWN(launcher.addMainWindow(NULL, &mainWindow), IllegalArgumentException);
     //QVERIFY_EXCEPTION_THROWN(launcher.addMainWindow(m_config->getDefaultProfile(), NULL), IllegalArgumentException);
     launcher.addMainWindow(m_config->getDefaultProfile(), m_mainWindow);
     launcher.start();
-    //QVERIFY(launcher.alreadyRun() == true);
     QVERIFY(launcher.profileIsLoad(m_config->getDefaultProfile()) != NULL);
     launcher.UnloadProfileWithWindow(m_config->getDefaultProfile());
     launcher.stop();
     while(launcher.isRunning());
-    //QVERIFY(launcher.alreadyRun() == true);
+    QVERIFY(launcher.alreadyRun() == false);
+    qDebug() << "Classe Launcher : OK!";
 }
 
-void MainTest::testCase_u7()
+void MainTest::testCase_u5()
 {
     QStringList interactions = (QStringList() << "1" << "2" << "3");
     Action a("test", QStringList(), QStringList(), interactions);
@@ -133,76 +146,50 @@ void MainTest::testCase_u7()
     QVERIFY(a.nextInteraction() == "3\n");
     //Vérifier que a.nextInteraction() lance une exception (qt 5.3 requis)
     //QVERIFY_EXCEPTION_THROWN(a.nextInteraction(), IllegalStateException);
+    qDebug() << "Classe Action : OK!";
 }
 
-void MainTest::testCase_u8()
+void MainTest::testCase_u6()
 {
     GPGManager manager(m_config->getDefaultProfile());
     Action a("--version", QStringList(), QStringList());
     manager.setAction(a);
     manager.execute();
-
+    qDebug() << "Classe GPGManager : OK!";
 }
 
-void MainTest::testCase_u10()
+void MainTest::testCase_u7()
+{
+    PrimaPubKey primaPubKey(QString(), QString(""), 100, QString(), QString(), QDate(1,2,3), QDate(1,2,3), QString(), QString());
+    QVERIFY(primaPubKey.getTrust() == QString(""));
+    SubPubKey* subPubKey = new SubPubKey(QString(), QString(), 10, QString(), QString(), QDate(1,2,3), QDate(2,3,4), QString());
+    primaPubKey.addSubPubKey(subPubKey);
+    QVERIFY(primaPubKey.getSubPubKeyList().contains(subPubKey));
+    Uid* uid = new Uid("1", QDate(1,2,3), QString(""), QString("bob"), QString("bob@bob.bob"));
+    primaPubKey.addUid(uid);
+    QVERIFY(primaPubKey.getUidList().contains(uid));
+    primaPubKey.setPrimaryUid(uid);
+    QVERIFY(primaPubKey.getPrimaryUid() == uid);
+    QVERIFY(primaPubKey.hasPrimaSecKey() == false);
+    PrimaSecKey* primaSecKey= new PrimaSecKey(QString("1"), 1024, QString("aaa"), QString("6666666"), QDate(1,2,3), QDate(4,5,6));
+    primaPubKey.setPrimarySecKey(primaSecKey);
+    QVERIFY(primaPubKey.hasPrimaSecKey() == true);
+    QVERIFY(primaPubKey.getPrimarySecKey() == primaSecKey);
+    qDebug() << "Classe PrimaPubKey : OK!";
+}
+
+void MainTest::testCase_u8()
 {
 
     QVERIFY(m_model->getLauncher() == m_launcher);
     QVERIFY(m_model->getGuiPGApp() == m_app);
     QVERIFY(m_model->getConf() == m_config);
     QVERIFY(m_model->getProfile() == m_config->getDefaultProfile());
-    //m_model->loadProfile(m_config->getDefaultProfileId(), m_mainWindow);
-    //m_model->initKeyManager();
     QVERIFY(m_model->getKeyManager() != NULL);
+    qDebug() << "Classe MainWindowModel : OK!";
 }
 
-void MainTest::testCase_u11()
-{
-    //A FAIRE A LA MAIN
-    /*remove("/tmp/TEST");
-    KeyExport keyExport(m_mainWindow);
-    keyExport.exportFunction(KeyExport::EXPORT_FILE, "", "/tmp/TEST");
-    QVERIFY(remove("/tmp/TEST") != -1);
-    QVERIFY(keyExport.exportFunction(KeyExport::EXPORT_FILE, "", "/tmp/gaijgz/kjqnonzb/zzozop") == -1);
-    QVERIFY(remove("/tmp/gaijgz/kjqnonzb/zzozop") == -1);
-    QVERIFY(keyExport.exportFunction(KeyExport::EXPORT_FILE, "", "/etc/test") == -1);
-    QVERIFY(remove("/etc/test") == -1);*/
-}
 
-void MainTest::testCase_u12()
-{
-    //A FAIRE A LA MAIN
-
-}
-
-void MainTest::testCase_nr1()
-{
-    //A FAIRE A LA MAIN
-    /*remove("/tmp/TEST");
-    KeyExport* keyExport = new KeyExport(m_mainWindow);
-    keyExport->exportFunction(KeyExport::EXPORT_FILE, "", "/tmp/TEST");
-    QVERIFY(remove("/tmp/TEST") != -1);*/
-}
-
-void MainTest::testCase_nr2()
-{
-    //A FAIRE A LA MAIN
-    /*Profile* p = new Profile(1234, "test");
-    p->setConfigurationPath("/tmp");
-    p->setGPGExecutable("/usr/bin/gpg");
-    m_config->addProfile(p);
-    for (int i=0; i < 20; i++) {
-        m_mainWindow->changeProfil(1);
-        m_mainWindow->changeProfil(1234);
-    }
-    m_config->deleteProfile(p->getId());*/
-}
-
-void MainTest::testLoadConfig()
-{
-    Configuration config("../config.xml");
-    QVERIFY(config.load() == true);
-}
 
 void MainTest::cleanupTestCase() {
     m_config->setDefaultProfileId(1);
