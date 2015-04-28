eee02bb4cf5d830fa5bd070e76c51f4baec51137
Olivier THIBAULT <olivier.thibault@ymail.com>
2015-04-28 19:39:46 +0200

    maj chiffrement, vérifications des droits des fichiers etc... et SELECTION DE PLUSIEURS DESTINATAIRES ET AUSSI DE MANIERE ANONYME git add *git add *git add *

--- GuiPG/src/View/fileencryption.cpp
+++ GuiPG/src/View/fileencryption.cpp
@@ -18,7 +18,30 @@ FileEncryption::FileEncryption(MainWindow *parent, KeyManager* keyManager) :
     m_parent = parent;
     m_keyManager = keyManager;
 
+    ui->tableWidgetRecipient->setColumnCount(7);
+    ui->tableWidgetRecipient->verticalHeader()->setVisible(false);
+    ui->tableWidgetRecipient->setEditTriggers(QAbstractItemView::NoEditTriggers);
+    QStringList m_TableHeader;
+    m_TableHeader   <<"Nom"
+                    <<"Email"
+                    <<"Empreinte utilisateur"
+                    <<"Validité utilisateur"
+                    <<"Identifiant clef"
+                    <<"Validitié clef"
+                    <<"Confiance";
+    ui->tableWidgetRecipient->setHorizontalHeaderLabels(m_TableHeader);
+    ui->tableWidgetRecipient->setColumnWidth(0,150);
+    ui->tableWidgetRecipient->setColumnWidth(1,150);
+    ui->tableWidgetRecipient->setColumnWidth(2,150);
+    ui->tableWidgetRecipient->setColumnWidth(3,130);
+    ui->tableWidgetRecipient->setColumnWidth(4,150);
+    ui->tableWidgetRecipient->setColumnWidth(5,100);
+    ui->tableWidgetRecipient->setColumnWidth(6,90);
+
+    int i = 0;
+
     for (PrimaPubKey* key : keyManager->getPubKeys()) {
+
         if ((key->getValidity() == VALIDITY_NO_VALUE ||
                     key->getValidity() == VALIDITY_UNKNOWN ||
                     key->getValidity() == VALIDITY_UNDEFINED ||
@@ -28,18 +51,33 @@ FileEncryption::FileEncryption(MainWindow *parent, KeyManager* keyManager) :
                     (key->getCapabilities().contains("e") ||
                      key->getCapabilities().contains("E"))) {
 
+            for (Uid* uid : key->getUidList()) {
 
+                if (uid->getValidity() == VALIDITY_NO_VALUE ||
+                        key->getValidity() == VALIDITY_UNKNOWN ||
+                        key->getValidity() == VALIDITY_UNDEFINED ||
+                        key->getValidity() == VALIDITY_MARGINAL ||
+                        key->getValidity() == VALIDITY_FULLY ||
+                        key->getValidity() == VALIDITY_ULTIMATELY) {
+
+                    ui->tableWidgetRecipient->setRowCount(i+1);
+                    ui->tableWidgetRecipient->setRowHeight(i, 20);
+                    ui->tableWidgetRecipient->setItem(i, 0, new QTableWidgetItem(uid->getName()));
+                    ui->tableWidgetRecipient->setItem(i, 1, new QTableWidgetItem(uid->getMail()));
+                    ui->tableWidgetRecipient->setItem(i, 2, new QTableWidgetItem(uid->getFpr().right(16)));
+                    ui->tableWidgetRecipient->setItem(i, 3, new QTableWidgetItem(GpgObject::validityToStr(uid->getValidity())));
+                    ui->tableWidgetRecipient->setItem(i, 4, new QTableWidgetItem(key->getKeyId()));
+                    ui->tableWidgetRecipient->setItem(i, 5, new QTableWidgetItem(GpgObject::validityToStr(key->getValidity())));
+                    ui->tableWidgetRecipient->setItem(i, 6, new QTableWidgetItem(PrimaPubKey::trustToStr(key->getTrust())));
+                    ui->tableWidgetRecipient->item(i, 6)->setBackgroundColor(Configuration::getDefaultValidityColor(key->getTrust()));
+
+                    ui->tableWidgetRecipient->item(i, 3)->setBackgroundColor(Configuration::getDefaultValidityColor(uid->getValidity()));
+                    ui->tableWidgetRecipient->item(i, 5)->setBackgroundColor(Configuration::getDefaultValidityColor(key->getValidity()));
+                    i++;
+                }
+            }
         }
     }
-
-    //QList<PrimaPubKey *> pubKeys = keyManager->getPubKeys();
-    //QList<Uid *>* uids = new QList<Uid *>();
-    //for (PrimaPubKey* key : pubKeys) {
-    //    uids->append(key->getUidList());
-    //}
-    //for (Uid* uid : *uids) {
-    //    ui->comboBox->addItem(uid->getName());
-    //}
 }
 
 FileEncryption::~FileEncryption()
@@ -49,8 +87,8 @@ FileEncryption::~FileEncryption()
 
 void FileEncryption::on_browseButton_clicked()
 {
-    QString pathName = QFileDialog::getOpenFileName(this, "Fichier cible");
-    ui->pathEdit->setText(pathName);
+    QString pathName = QFileDialog::getOpenFileName(this, "Selection du fichier à chiffrer");
+    ui->sourceFileEdit->setText(pathName);
 }
 
 void FileEncryption::on_exitButton_clicked()
@@ -61,30 +99,111 @@ void FileEncryption::on_exitButton_clicked()
 
 void FileEncryption::onEncryptionCompleted()
 {
-    ui->errorLabel->setText("Chiffrement terminé !");
+    ui->warningLabel->setText("Chiffrement terminé !");
     ui->exitButton->setText("Fermer");
 }
-
+#include <QDebug>
 void FileEncryption::on_okButton_clicked()
 {
-    if (ui->outputEdit->text() == ui->pathEdit->text()) {
-        ui->errorLabel->setText("Vous ne pouvez pas réécrire le fichier.");
+    ui->warningLabel->setText("");
+    if (ui->sourceFileEdit->text().startsWith("~/") || ui->destinationFileEdit->text().startsWith("~/")) {
+        ui->warningLabel->setText("Seul les chemins absolus sont autorisés.");
         return;
     }
-    struct stat st;
-    QByteArray ba = ui->outputEdit->text().toLocal8Bit();
-    const char *file = ba.data();
-    if(stat(file, &st) == 0) {
-        remove(file);
+
+    if (ui->sourceFileEdit->text() == ui->destinationFileEdit->text()) {
+        ui->warningLabel->setText("Vous ne pouvez pas utiliser le même fichier source et destination.");
+        return;
     }
-    //Action action(QString("-e"), QStringList() << ui->pathEdit->text(), QStringList("--command-fd=0") << "--status-fd=1" << "--output" << ui->outputEdit->text(), QStringList(ui->comboBox->currentText()) << "");
-    //m_manager->setAction(action);
-    //connect(m_manager, &GPGManager::finished, this, &FileEncryption::onEncryptionCompleted);
-    //m_manager->execute();
+
+    QFile sourcefile(ui->sourceFileEdit->text());
+    if (!sourcefile.exists()) {
+        ui->warningLabel->setText("Le fichier source n'existe pas.");
+        return;
+    }
+
+    QFileInfo sourceFileInfo(sourcefile);
+    if (!sourceFileInfo.isReadable()) {
+        ui->warningLabel->setText("Vous n'avez pas les droits de lecture sur le fichier source.");
+        return;
+    }
+
+    QFile destinationfile(ui->destinationFileEdit->text());
+    if (destinationfile.exists()) {
+        if (!destinationfile.remove()) {
+            ui->warningLabel->setText("Impossible d'écraser le fichier existant. Veuillez modifier la destination.");
+            return;
+        }
+    }
+
+    QFileInfo destinationFileInfo(destinationfile);
+    QFileInfo destinationDirInfo(destinationFileInfo.absoluteDir().path());
+    if (!destinationDirInfo.isWritable()) {
+        ui->warningLabel->setText("Vous n'avez pas les droits d'écriture sur le dossier destination.");
+        return;
+    }
+
+    // On récupère l'indice des lignes selectionnées
+    QModelIndexList selectedIndex = ui->tableWidgetRecipient->selectionModel()->selectedIndexes();
+    QList<int> rowIndexes;
+    for (QModelIndex index : selectedIndex) {
+        if (!rowIndexes.contains(index.row())) {
+            rowIndexes.append(index.row());
+        }
+    }
+    /* Utilisé pour paramétré les intéractions car gpg est tellement
+     * intelligent qu'il les demandes dans l'ordre inverse pour le cas anonyme
+     */
+    QList<int> reverseRowIndexes;
+    reverseRowIndexes.reserve(rowIndexes.size());
+    std::reverse_copy(rowIndexes.begin(), rowIndexes.end(), std::back_inserter(reverseRowIndexes));
+
+    QStringList opt;
+    opt << "--command-fd=0"
+        << "--status-fd=1"
+        << "--output" << ui->destinationFileEdit->text();
+    QString cmd = "-e";
+    QStringList arg;
+    arg << ui->sourceFileEdit->text();
+    QStringList interactions;
+
+    if (ui->anonymousCheckBox->isChecked()) {
+        for (int i : rowIndexes) {
+            opt << "-R \""
+                   + ui->tableWidgetRecipient->item(i, 0)->text()
+                   + " <"
+                   + ui->tableWidgetRecipient->item(i, 1)->text()
+                   + ">\"";
+        }
+
+        for (int i : reverseRowIndexes) {
+            if (ui->tableWidgetRecipient->item(i, 3)->text() == GpgObject::validityToStr(VALIDITY_NO_VALUE) ||
+                    ui->tableWidgetRecipient->item(i, 3)->text() == GpgObject::validityToStr(VALIDITY_UNDEFINED)) {
+                interactions << "y";
+            }
+        }
+    } else {
+        for (int i : rowIndexes) {
+            interactions << ui->tableWidgetRecipient->item(i, 0)->text()
+                           + " <"
+                           + ui->tableWidgetRecipient->item(i, 1)->text()
+                           + ">";
+            if (ui->tableWidgetRecipient->item(i, 3)->text() == GpgObject::validityToStr(VALIDITY_NO_VALUE) ||
+                    ui->tableWidgetRecipient->item(i, 3)->text() == GpgObject::validityToStr(VALIDITY_UNDEFINED)) {
+                interactions << "y";
+            }
+        }
+        interactions << "";
+    }
+
+    Action action(cmd, arg, opt, interactions);
+    m_manager->setAction(action);
+    connect(m_manager, &GPGManager::finished, this, &FileEncryption::onEncryptionCompleted);
+    m_manager->execute();
 }
 
 void FileEncryption::on_outputButton_clicked()
 {
     QString pathName = QFileDialog::getSaveFileName(this, "Fichier chiffré");
-    ui->outputEdit->setText(pathName);
+    ui->destinationFileEdit->setText(pathName);
 }
